7e1791cc935efc5c917cd0754ff6247ae27cc6a7
James Guerra <james.guerra@medmate.com.au>
2023-08-23 14:53:50 +1000

    created auth controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\LoginRequest;
+use App\Http\Requests\Api\RegisterRequest;
+use App\Http\Resources\UserResource;
+use App\User;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $data = $request->validated();
+
+        $data['password'] = bcrypt($request->password);
+
+        $user = UserResource::make(User::create($data));
+
+        $token = $user->createToken(config('app.name'))->accessToken;
+
+        return response()->json(['user' => $user, 'token' => $token]);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        if (!auth()->attempt($request->validated())) {
+            return response()->json(['success' => 1]);
+        }
+
+        $user = auth()->user();
+        $token = $user->createToken(config('app.name'))->accessToken;
+
+        return response()->json(['user' => UserResource::make($user), 'token' => $token]);
+    }
+}
